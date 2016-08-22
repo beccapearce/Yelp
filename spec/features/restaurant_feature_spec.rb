2a6ba56098ca72bf4d247cfe3e522de469e8c2b0
Rebecca Pearce <becciepearce@hotmail.co.uk>
2016-08-22 17:57:00 +0100

    Sets up rail

---
+++ spec/features/restaurant_feature_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+feature 'restaurants' do
+  context 'no restaurant have been added' do
+    scenario 'should display a prompt to add a restaurant' do
+      visit '/restaurants'
+      expect(page).to have_content 'No restaurants yet'
+      expect(page).to have_link "Add a restaurant"
+    end
+    context 'restaurants have been added' do
+      before do
+        Restaurant.create(name: 'KFC')
+      end
+
+      scenario 'display restaurants' do
+        visit '/restaurants'
+        expect(page).to have_content('KFC')
+        expect(page).not_to have_content('No restaurants yet')
+      end
+      context 'creating reatuants' do
+        scenario 'prompts user to fill out a form, then displays the new restaurant' do
+          visit '/restaurants'
+          click_link 'Add a restaurant'
+          fill_in 'Name', with: 'KFC'
+          click_button 'Create Restaurant'
+          expect(page).to have_content 'KFC'
+          expect(current_path).to eq '/restaurants'
+        end
+      end
+      context 'viewing restaurants' do
+
+        let!(:kfc){ Restaurant.create(name:'KFC') }
+
+        scenario 'lets a user view a restaurant' do
+          visit '/restaurants'
+          click_link 'KFC'
+          expect(page).to have_content 'KFC'
+          expect(current_path).to eq "/restaurants/#{kfc.id}"
+        end
+      end
+    end
+  end
+end
